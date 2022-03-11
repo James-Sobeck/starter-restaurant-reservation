30a6def29f1c213e478bc0502c151df07d28faad
James Sobeck <sobeckjames@gmail.com>
2022-03-10 18:05:02 -0600

    frontend

--- front-end/src/reservations/Reservations.js
+++ front-end/src/reservations/Reservations.js
@@ -1,38 +1,65 @@
-import React, { useEffect, useState } from "react";
-import { listReservations } from "../utils/api";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { createReservation } from "../utils/api";
+import { isNotOnTuesday } from "../utils/date-time";
+import { isInTheFuture } from "../utils/date-time";
 import ErrorAlert from "../layout/ErrorAlert";
+import Form from "./Form";
 
-export default function Reservations({ date }){
-    return (<>
-        <div>
-            <form>
-                <label>First Name:</label>
-                <input
-                name="first_name"
-                required/>
-                <label>Last Name:</label>
-                <input
-                name="last_name"
-                required/>
-                <label>Mobile Number:</label>
-                <input
-                name="mobile_number"
-                required/>
-                <label>Date of Reservation:</label>
-                <input
-                name="reservation_date"
-                required/>
-                <label>Time of Reservation:</label>
-                <input
-                name="reservation_time"
-                required/>
-                <label>Number of people in the party:</label>
-                <input
-                name="people"
-                required/>
-                <button>Submit</button>
-                <button>Cancel</button>
-            </form>
-        </div>
-    </>)
+export default function Reservations() {
+  const history = useHistory();
+  const [reservationsError, setReservationsError] = useState(null);
+  const initialFormData = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: 0,
+  };
+
+  const [formData, setFormData] = useState({ ...initialFormData });
+
+  const handleFormChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const findErrors = (date, errors) => {
+    isNotOnTuesday(date, errors);
+    isInTheFuture(date, errors);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const controller = new AbortController();
+    const errors = [];
+    findErrors(formData.reservation_date, errors);
+    if (errors.length) {
+      setReservationsError({ message: errors });
+      return;
+    }
+    try {
+      formData.people = Number(formData.people);
+      await createReservation(formData, controller.signal);
+      const date = formData.reservation_date;
+      history.push(`/dashboard?date=${date}`);
+    } catch (error) {
+      setReservationsError(error);
+    }
+    return () => controller.abort();
+  };
+
+  return (
+    <>
+      <ErrorAlert error={reservationsError} />
+      <Form
+        initialformData={formData}
+        handleFormChange={handleFormChange}
+        handleSubmit={handleSubmit}
+      />
+    </>
+  );
 }
